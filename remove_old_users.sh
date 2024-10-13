09ab96d46cbaf49b3da514d3bd882fb1bbdbed10
Muhammed Mustafa Akşam <info@muhammedaksam.com.tr>
2024-10-13 13:25:54 +0300

    Fix: Refactor user and group setup scripts

--- remove_old_users.sh
+++ remove_old_users.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# shut down all ezarr containers referenced by docker-compose.yml 
+# Shut down all ezarr containers referenced by docker-compose.yml 
 # If you previously had ezarr installed somewhere else you have to do this manually in the directory where the docker-compose file is.
 sudo docker compose down
 
 # Remove old users and group
 for user in sonarr radarr lidarr readarr mylar audiobookshelf bazarr prowlarr jackett plex overseerr jellyseerr qbittorrent sabnzbd; do
     if id "$user" &>/dev/null; then
-        sudo userdel "$user" &>/dev/null
+        sudo userdel "$user"
         echo "Deleted user: $user"
+    else
+        echo "User $user does not exist"
     fi
 done
 
 if getent group mediacenter &>/dev/null; then
     sudo groupdel mediacenter
     echo "Deleted group: mediacenter"
+else
+    echo "Group mediacenter does not exist"
 fi
